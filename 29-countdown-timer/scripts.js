b71801cdd73ab49a8adfb5a9915053562030ed6d
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-10-24 22:17:05 -0200

    Add start and stop button to countdown timer

--- 29-countdown-timer/scripts.js
+++ 29-countdown-timer/scripts.js
@@ -1,17 +1,26 @@
 let countdown;
+let isPaused = false;
 const timerDisplay = document.querySelector('.display-time-left');
 const endTime = document.querySelector('.display-end-time');
 const buttons = document.querySelectorAll('[data-time]');
+const pauseButton = document.querySelector('#pause');
+const stopButton = document.querySelector('#stop');
 
 function timer(seconds) {
 	clearInterval(countdown);
 	const now = Date.now();
 	const then = now + (seconds * 1000);
+	let timePaused = 0;
 
 	displayEndTime(then);
 	displayTimeLeft(seconds);
 	countdown = setInterval(() => {
-		const secondsLeft = Math.round((then - Date.now()) / 1000);
+		if (isPaused) {
+			timePaused++;
+			return;
+		}
+		const secondsLeft = Math.round((then - Date.now() + (timePaused * 1000)) / 1000);
+		displayEndTime(then + (timePaused * 1000));
 		displayTimeLeft(secondsLeft);
 		if (secondsLeft <= 0) {
 			clearInterval(countdown);
@@ -48,9 +57,23 @@ function startTime() {
 	timer(parseInt(this.dataset.time));
 }
 
+function togglePause() {
+	pauseButton.textContent = isPaused ? 'Pause' : 'Resume';
+	isPaused = !isPaused;
+}
+
+function stopTimer() {
+	clearInterval(countdown);
+	setTimeout(() => timerDisplay.textContent = '00:00:00', 0);
+	document.title = 'Timer';
+	endTime.textContent = '';
+}
+
 buttons.forEach((button) => button.addEventListener('click', startTime));
-document.customForm.addEventListener('submit', function(e) {
+document.customForm.addEventListener('submit', function (e) {
 	e.preventDefault();
 	timer(this.minutes.value * 60);
 	this.reset();
 });
+pauseButton.addEventListener('click', togglePause);
+stopButton.addEventListener('click', stopTimer);
